3fbcd2a724a1445f65622154381f3d836d4316b8
duttrohan0302 <duttrohan0302@gmail.com>
2020-05-07 04:25:44 +0530

    First part of react done

--- validation/register.js
+++ validation/register.js
@@ -23,11 +23,12 @@ module.exports = function validateRegisterInput(data) {
   if (!Validator.isEmail(data.email)) {
     errors.email = "Email is invalid";
   }
-  if (Validator.isEmpty(data.password)) {
-    errors.password = "Password field is required";
-  }
+
   if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
     errors.password = "Password must be at least 6 characters";
+  }
+  if (Validator.isEmpty(data.password)) {
+    errors.password = "Password field is required";
   }
   if (Validator.isEmpty(data.password2)) {
     errors.password2 = "Confirm Password field is required";
