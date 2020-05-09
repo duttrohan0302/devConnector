989c41a8886d3c5c3123b598c850b4eb8a851de7
duttrohan0302 <duttrohan0302@gmail.com>
2020-05-09 19:17:40 +0530

    Complete project DevConnector

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://duttrohan0302:719430L.@devconnector-wws0c.mongodb.net/test?retryWrites=true&w=majority",
-  secretOrKey: "secretforlogin",
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
